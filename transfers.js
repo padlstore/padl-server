544a65941a9dda45ae24da78d13e1158466cee2d
Matthew Feng <mattfeng@mit.edu>
2018-06-28 14:07:06 -0400

    Added comments

--- transfers.js
+++ transfers.js
@@ -6,17 +6,21 @@ var db = admin.database()
 var offers = db.ref('offers')
 var users = db.ref('users')
 
+/* The listener to perform transfer when both parties have signed contract */
 offers.on('child_changed', (snap) => {
+  // Listen for changes
   var change = snap.val()
 
   if (change === null) {
     return
   }
 
+  // If buyer and seller have signed, and transfer hasn't been performed
   if (change.buyerSigned && change.sellerSigned &&
     !change.performedTransfer) {
     let sellerRef = users.child(change.seller)
 
+    // Get the seller's info (namely, seller.stripeAccountId)
     sellerRef.once('value', (snap_) => {
       let seller = snap_.val()
 
@@ -29,7 +33,8 @@ offers.on('child_changed', (snap) => {
         'amount': change.price,
         'currency': 'usd',
         'source_transaction': change.chargeId,
-        'destination': seller.stripeAccountId
+        'destination': seller.stripeAccountId,
+        'description': 'Transfer to seller of offer ' + change.offerId
       }).then((transfer) => {
         return offers.child(change.offerId).update({
           'transferId': transfer.id,
